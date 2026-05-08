f8a2bc515095dcf85afa143ac2687cbf15d840e7
HardenPower <a.boot96@gmail.com>
2026-05-08 22:30:06 +0300

    7. Балансир [#505083]

---
+++ src/main/java/ru/job4j/iterator/Balancer.java
@@ -0,0 +1,20 @@
+package ru.job4j.iterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class Balancer {
+    public static void split(List<ArrayList<Integer>> nodes, Iterator<Integer> source) {
+        if (nodes == null || nodes.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        int index = 0;
+        while (source.hasNext()) {
+            Integer elem = source.next();
+            nodes.get(index).add(elem);
+            index = (index + 1) % nodes.size();
+        }
+    }
+}
